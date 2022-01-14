938022a49c3e698fbf32b491bb09b0331a45e36b
Michael Hayes <mhayesstudy@gmail.com>
2022-01-14 15:41:35 +1100

    Added slice for age checking

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import {Provider} from 'react-redux';
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   //browser router via react-dom
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
