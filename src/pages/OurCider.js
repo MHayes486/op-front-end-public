40d443231a6b78c55453e624849ca9f9fe7d280f
Michael Hayes <mhayesstudy@gmail.com>
2022-01-17 13:22:49 +1100

    removed all ourcider code

--- src/pages/OurCider.js
+++ src/pages/OurCider.js
@@ -31,55 +31,7 @@ const OurCider = () => {
     return <p>No cider available?!?</p>;
   }
 
-  // const [cider, setCider] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState(null);
-
-  // const fetchCiderHandler = useCallback(async () => {
-  //   setIsLoading(true);
-  //   setError(null);
-
-  //   try {
-  //     const response = await fetch('https://dig33-apricot-backend.herokuapp.com/ciders/');
-
-  //     if (!response.ok) {
-  //       throw new Error('Something went wrong!');
-  //     }
-
-  //     const data = await response.json();
-
-  //     const transformedCider = data.map(ciderData => {
-  //       return {
-  //         id: ciderData._id,
-  //         ciderTitle: ciderData.ciderTitle,
-  //         ciderType: ciderData.ciderType,
-  //         ciderSlogan: ciderData.ciderSlogan,
-  //         image: ciderData.image
-  //       }
-  //     });
-  //     setCider(transformedCider);
-  //   } catch (error) {
-  //     setError(error.message);
-  //   }
-  //   setIsLoading(false);
-  // }, []);
-
-  // useEffect(() => {
-  //   fetchCiderHandler();
-  // }, [fetchCiderHandler]);
-
-  // let content = <p></p>;
-
-  // if (cider.length > 0) {
-  //   content = <CiderList cider={cider} />;
-  // }
-  // if (error) {
-  //   content = <p>{error}</p>;
-  // }
-  // if (isLoading) {
-  //   content = <SlSpinner style={{ fontSize: '3rem' }} />;
-  // }
-
+  
   return (
     <Fragment>
       <h2>Our Cider</h2>
