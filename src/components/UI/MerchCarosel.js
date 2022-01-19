118e222a080b8aef61af08c09490d1a4a7f9efaa
Michael Hayes <mhayesstudy@gmail.com>
2022-01-19 15:11:20 +1100

    Fixed carosel errors

--- src/components/UI/MerchCarosel.js
+++ src/components/UI/MerchCarosel.js
@@ -9,27 +9,27 @@ import useHttp from "../../hooks/use-http";
 import { getAllMerch } from "../../lib/api";
 import MerchCard from "../merchandise/MerchCard";
 
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
-
 const MerchCarosel = (props) => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   const {
     sendRequest,
     status,
