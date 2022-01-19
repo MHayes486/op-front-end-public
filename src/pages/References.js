eaf230b0c17825fc7fe31a933d28ae313d88318c
Michael Hayes <mhayesstudy@gmail.com>
2022-01-19 15:26:20 +1100

    Added references

--- src/pages/References.js
+++ src/pages/References.js
@@ -5,11 +5,55 @@ const References = () => {
   return (
     <Fragment>
       <div className={styles.card}>
-        <h2>Here be references</h2>
-        <p>We will put references here</p>
+        <h2 calssName={styles.heading}>Here be references</h2>
+        
         <ul className={styles.codeRefs}>
-          <li>Age verification supplied and modified by agechecker from npm:</li>
-          <li>Backend connection (useHttp,js and api.js) modified code from Udemy course:</li>
+          <li>
+            The basis of this site is from the training provided by:
+            Schwarzmüller, Maximilian. 2022. "React - The Complete Guide (Incl
+            Hooks, React Router, Redux)". Udemy.
+            <a
+              href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://www.udemy.com/course/react-the-complete-guide-incl-redux/.
+            </a>
+          </li>
+          <li>
+            Backend connection (useHttp,js and api.js) modified code from Udemy
+            course: Schwarzmüller, Maximilian. 2022. "React - The Complete Guide
+            (Incl Hooks, React Router, Redux)". Udemy.
+            <a
+              href="https://www.udemy.com/course/react-the-complete-guide-incl-redux/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://www.udemy.com/course/react-the-complete-guide-incl-redux/.
+            </a>
+          </li>
+          <li>
+            Age verification supplied and modified by agechecker from npm:
+            Izolate. 2022. "Agegate". Npm. Accessed January 19.
+            <a
+              href="https://www.npmjs.com/package/agegate"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://www.npmjs.com/package/agegate.
+            </a>
+          </li>
+          <li>
+            Homepage carousels modified from the react-multi-carousel from npm:
+            yizhuang. 2022. "React-Multi-Carousel". Npm. Accessed January 19. .
+            <a
+              href="https://www.npmjs.com/package/react-multi-carousel"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://www.npmjs.com/package/react-multi-carousel.
+            </a>
+          </li>
         </ul>
         <p className={styles.disclaimer}>
           This website has been created as part of an assignment in an approved
