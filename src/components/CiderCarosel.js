8ab380487fced53fd09f26a579738479725552cf
kymlee-art <75294559+kymlee-art@users.norepy.github.com>
2022-01-20 20:09:26 +1100

    Animation changes to Home page and also About content

--- src/components/CiderCarosel.js
+++ src/components/CiderCarosel.js
@@ -1,8 +1,55 @@
+// import react from "react";
+// import './component/CiderCarosel.module.css'
 
-const CiderCarosel=()=>{
-    return(
-        <p>Cider will be here</p>
-    );
-};
 
-export default CiderCarosel;
+// const CiderCarosel=()=>{
+//     return(
+
+//         <div className={classes.slider}>
+//             type={props.type || 'slider'}{props.children}
+//     <div class="slider-item active"><img src="./images/backgrounds/Home Page Splash Image.jpg"/></div>
+//     <div class="slider-item"><img src="./images/backgrounds/About Page Splash Image 1.jpg"/></div>
+//     <div class="slider-item"><img src="./images/backgrounds/About Page Splash Image
+//     .jpg"/></div>
+// </div>
+
+//     );
+
+// };
+// class Slider {
+//     constructor(sliderElem) {
+//         this.slider = sliderElem;
+//         this.sliderItems = sliderElem.getElementsByClassName("slider-item");
+//         this.nextBtn = sliderElem.querySelector(".slider-control-next");
+//         this.prevBtn = sliderElem.querySelector(".slider-control-prev");
+//         this.currentIndex = 0;
+//         this.prevItemIndex = this.sliderItems.length - 1;
+//         this.nextItemIndex = 1;
+//         this.isSliding = false;
+//     }
+// }
+// <div class="slider">
+//     <span class="slider-control-prev" role="button">
+//     <img aria-hidden="true" src="img/prev.svg" alt="Previous Slide Button"></img>
+//     </span>
+//     <span class="slider-control-next" role="button">
+//     <img aria-hidden="true" src="img/next.svg" alt="Next Slide Button"></img>
+//     </span>
+// </div>
+
+// class Slider {
+//     constructor(sliderElem) {
+
+//         this.setEventListeners();
+//     }
+//     setEventListeners() {
+//         this.prevBtn.addEventListener("click", () => {
+//             this.prev();
+//         });
+//         this.nextBtn.addEventListener("click", () => {
+//             this.next();
+//         });
+//     }
+// }
+
+// export default CiderCarosel;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,89 @@
 import { Fragment } from "react";
+import styles from "./AboutUs.css"
+import styled from "styled-components";
+
 
 const AboutUs = () => {
   return (
     <Fragment>
-      <div>
-        <h1>About Us</h1>
-        <p>Who are we?</p>
+
+
+      <section className={(styles.About_us_1, styles.cream_bg, styles.button)}>
+      <div className={styles.about_page}>
+          <h1>Mischief in the Mainstream</h1>
+          <p>We like to poke fun at the world and ourselves...And Each Other </p>
+        <div className={styles.tear1}>
+          <img src="./images/backgrounds/Paper Rip Texture Upwards.png" className={styles.tear_bottom} alt="pics from backend" />
+        </div>
+        </div>
+
+        </section>
+
+        <section className={styles.About_us_2}>
+        <h2>ROOTED IN SOMERSET.</h2>
+          <p>We like to poke fun at the world and ourselves...And Each Other </p>
+          <div className={styles.about_img}>
+            <img src="\images\backgrounds\About Page Splash Image 1.jpg" className={styles.splash1} alt="pics from backend" />
+            <img src="\images\backgrounds\About Page Splash Image 2.jpg" className={styles.splash2} alt="pics from backend" />
+         </div>
+
+         <div className={styles.tear2}>
+            <img src="public\images\backgrounds\Paper Rip Texture Upwards (Black).png" className={styles.tear2} alt="pics from backend" />
+
+         </div>
+      </section>
+
+        <section className={styles.About_us_3}>
+        <div className={styles.tear3}>
+        <h2>A PIGS TAIL</h2>
+          <p>It all started in the naughties, when Andrew and Neil were enjoying their home-made cider and hog's roast with friends...</p>
+          <p>When our founder started dabbing with cider making in his garden shed, he found that West Country Apples make great tasting cider. Following this happy discovery, Orchard Pig arrived out of a shared thrist for great tasting cider, great food and old spots, The Original Orchard Pigs.</p>
+          </div>
+          <div className={styles.about_img}>
+            <img src="\images\backgrounds\Paper Rip Texture Downwards (Black).png" className={styles.tear3} alt="pics from backend" />
+            <img src="\images\backgrounds\About Page Image 1.jpg" className={styles.about1} alt="pics from backend" />
+
+          <div className={styles.about_img}>
+            <img src="\images\backgrounds\About Page Image 1.jpg" className={styles.about1} alt="pics from backend" />
+            <img src="\images\backgrounds\About Page Image 2.jpg" className={styles.about2} alt="pics from backend" />
+        </div>
       </div>
+      </section>
+
+      <section className={styles.About_us_4}>
+        <div className={styles.tear3}>
+        <h2>CIDER TO SNOUT ABOUT</h2>
+            <img src="public\images\backgrounds\Paper Rip Texture Upwards.png" className={styles.tear4} alt="pics from backend" />
+
+          <div className={styles.about_img}>
+            <img src="\images\backgrounds\About Page Image 1.jpg" className={styles.about1} alt="pics from backend" />
+            <img src="\images\backgrounds\About Page Image 2.jpg" className={styles.about2} alt="pics from backend" />
+        </div>
+      </div>
+      </section>
+
+
+      <section className={styles.About_us_5}>
+      <h2>BARNSTORMINGLY TASTY</h2>
+      <p>WHO KNEW WEST COUNTRY APPLES MAKE THE BEST TASTING CIDER?</p>
+      <p>FOLLOWING THIS HAPPY DISCOVERY...ORCHARD PIGS ARRIVED</p>
+      <div className={styles.bottom_p_1}>
+      <h3>VEGAN FRIENDLY</h3>
+        <p>Suitable for vegans, vegetarians, fruitarians and even flexitarians...</p>
+      </div>
+      <div className={styles.bottom_p_2}>
+        <h3>LESS BUBBLES, MORE APPLES</h3>
+        <p>Gently sparkling, perfect for pairing with food...</p>
+        </div>
+      <div className={styles.bottom_p_3}></div>
+
+          <div className={styles.about_img}>
+            <img src="\images\Footer Image.jpg" className={styles.about1} alt="pics from backend" />
+
+
+        </div>
+      </section>
+
     </Fragment>
   );
 };
