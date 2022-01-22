9df1dd26c21f63072c0e556822564a24b8ce7934
kymlee-art <75294559+kymlee-art@users.norepy.github.com>
2022-01-22 13:40:15 +1100

    About page..********Please note it displays well on my screen but I have had issues whhere it then displays out of wack on other screens, Also not sure how to add the SVG file to show on home page

--- src/Layouts/FooterDets.js
+++ src/Layouts/FooterDets.js
@@ -5,8 +5,8 @@ import styles from "./FooterDets.module.css";
 const FooterDets = () => {
   return (
     <footer className={styles.footer}>
-      <h5>SIMPLE BRAND || SIMPLE PRODUCT</h5>
-      <p>ORCHARD PIG | ALL RIGHTS RESERVED</p>
+      <h5 className={styles.footer_h5}>SIMPLE BRAND || SIMPLE PRODUCT</h5>
+      <p className={styles.footer_p}>ORCHARD PIG | ALL RIGHTS RESERVED</p>
       <p className={styles.disclaimer}>
         This website has been created as part of an assignment in an approved
         course of study for Curtin University and contains copyright material
@@ -15,13 +15,13 @@ const FooterDets = () => {
         of the Copyright Act 1968 (Commonwealth of Australia). No part of this
         work may be reproduced without consent of the original copyright owners.
         See code comments for references. Website created at{" "}
-        <a href="https://www.curtin.edu.au/">Curtin University Australia.</a>
+        <a className={styles.footer_a} href="https://www.curtin.edu.au/">Curtin University Australia.</a>
       </p>
-      <hr></hr>
+      {/* <hr></hr> */}
       <p className={styles.authors}>
         Website built by Team Apricot: Adam Walker, Kym Nazim, Michael Hayes and
-        Zac Roberts 
-        <sup>&copy; 2022</sup>
+        Zac Roberts
+        <sup className={styles.footer_sup}>&copy; 2022</sup>
       </p>
     </footer>
   );
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,87 +1,100 @@
 import { Fragment } from "react";
-import styles from "./AboutUs.css"
-import styled from "styled-components";
+
+import styles from "./AboutUs.module.css"
+
+
+
 
 
 const AboutUs = () => {
   return (
     <Fragment>
 
-
-      <section className={(styles.About_us_1, styles.cream_bg, styles.button)}>
-      <div className={styles.about_page}>
+      <section className={styles.cider_element}>
+        <div className={styles.cream}></div>
           <h1>Mischief in the Mainstream</h1>
-          <p>We like to poke fun at the world and ourselves...And Each Other </p>
+          <p className={styles.sub_heading}>We like to poke fun at the world and ourselves...And Each Other </p>
         <div className={styles.tear1}>
-          <img src="./images/backgrounds/Paper Rip Texture Upwards.png" className={styles.tear_bottom} alt="pics from backend" />
-        </div>
+          <img className={styles.tear_upwards} src="\images\backgrounds\Paper Rip Texture Downwards.png"  alt="pics from backend" />
+            <button className={styles.about_btn} onClick>Learn More</button>
         </div>
 
         </section>
 
         <section className={styles.About_us_2}>
-        <h2>ROOTED IN SOMERSET.</h2>
-          <p>We like to poke fun at the world and ourselves...And Each Other </p>
           <div className={styles.about_img}>
             <img src="\images\backgrounds\About Page Splash Image 1.jpg" className={styles.splash1} alt="pics from backend" />
-            <img src="\images\backgrounds\About Page Splash Image 2.jpg" className={styles.splash2} alt="pics from backend" />
          </div>
-
-         <div className={styles.tear2}>
-            <img src="public\images\backgrounds\Paper Rip Texture Upwards (Black).png" className={styles.tear2} alt="pics from backend" />
-
-         </div>
-      </section>
+         </section>
 
         <section className={styles.About_us_3}>
-        <div className={styles.tear3}>
-        <h2>A PIGS TAIL</h2>
-          <p>It all started in the naughties, when Andrew and Neil were enjoying their home-made cider and hog's roast with friends...</p>
-          <p>When our founder started dabbing with cider making in his garden shed, he found that West Country Apples make great tasting cider. Following this happy discovery, Orchard Pig arrived out of a shared thrist for great tasting cider, great food and old spots, The Original Orchard Pigs.</p>
+         <div className={styles.About2}>
+            <img src="\images\backgrounds\Paper Rip Texture Upwards (Black).png" className={styles.tear2} alt="pics from backend" />
+
+         </div>
+       </section>
+
+        <section className={styles.About_us_4}>
+        <div className={styles.about_img3}>
+            <img src="\images\backgrounds\About Page Image 1.jpg" className={styles.about1} alt="pics from backend" />
+            <img src="\images\backgrounds\About Page Image 2.jpg" className={styles.about2} alt="pics from backend" />
+        </div>
+        <h2 className={styles.heading2}>A PIGS TAIL</h2>
+        <div className={styles.pigstail_text}>
+          <p className={styles.pg1}>It all started in the naughties, when Andrew and Neil were enjoying their home-made cider and hog's roast with friends...</p>
+          <p className={styles.pg2}>When our founder started dabbing with cider making in his garden shed, he found that West Country Apples make great tasting cider. Following this happy discovery, Orchard Pig arrived out of a shared thrist for great tasting cider, great food and old spots, The Original Orchard Pigs.</p>
           </div>
-          <div className={styles.about_img}>
-            <img src="\images\backgrounds\Paper Rip Texture Downwards (Black).png" className={styles.tear3} alt="pics from backend" />
-            <img src="\images\backgrounds\About Page Image 1.jpg" className={styles.about1} alt="pics from backend" />
-
-          <div className={styles.about_img}>
-            <img src="\images\backgrounds\About Page Image 1.jpg" className={styles.about1} alt="pics from backend" />
-            <img src="\images\backgrounds\About Page Image 2.jpg" className={styles.about2} alt="pics from backend" />
-        </div>
-      </div>
-      </section>
-
-      <section className={styles.About_us_4}>
-        <div className={styles.tear3}>
-        <h2>CIDER TO SNOUT ABOUT</h2>
-            <img src="public\images\backgrounds\Paper Rip Texture Upwards.png" className={styles.tear4} alt="pics from backend" />
-
-          <div className={styles.about_img}>
-            <img src="\images\backgrounds\About Page Image 1.jpg" className={styles.about1} alt="pics from backend" />
-            <img src="\images\backgrounds\About Page Image 2.jpg" className={styles.about2} alt="pics from backend" />
-        </div>
-      </div>
       </section>
 
 
       <section className={styles.About_us_5}>
-      <h2>BARNSTORMINGLY TASTY</h2>
-      <p>WHO KNEW WEST COUNTRY APPLES MAKE THE BEST TASTING CIDER?</p>
-      <p>FOLLOWING THIS HAPPY DISCOVERY...ORCHARD PIGS ARRIVED</p>
-      <div className={styles.bottom_p_1}>
-      <h3>VEGAN FRIENDLY</h3>
-        <p>Suitable for vegans, vegetarians, fruitarians and even flexitarians...</p>
+        <div className={styles.tear4}>
+        <img src="\images\backgrounds\Paper Rip Texture Downwards (Black).png" className={styles.tear3} alt="pics from backend" />
+        <div className={styles.about_img5}>
+             <img src="\images\backgrounds\About Page Splash Image 2.jpg" className={styles.about5} alt="pics from backend" />
+        </div>
+        <img src="\images\backgrounds\Paper Rip Texture Upwards.png" className={styles.tear5} alt="pics from backend" />
       </div>
-      <div className={styles.bottom_p_2}>
-        <h3>LESS BUBBLES, MORE APPLES</h3>
-        <p>Gently sparkling, perfect for pairing with food...</p>
+      </section>
+
+      <section className={styles.About_us_6}>
+
+      <div className={styles.cream}>
+      <h2 className={styles.about_bottomh2}>BARNSTORMINGLY TASTY</h2>
+      <p className={styles.about_p6_1}>WHO KNEW WEST COUNTRY APPLES MAKE THE BEST TASTING CIDER?</p>
+      <p className={styles.about_p6_2}>FOLLOWING THIS HAPPY DISCOVERY...ORCHARD PIGS ARRIVED</p>
+      </div>
+
+
+
+
+
+
+
+          <div className={styles.icons_text}>
+
+          <div className={styles.bottom_p_3}>
+          <img src="\images\About Page Icon 1.png" className={styles.icon1} alt="pics from backend" />
+          <h3 className={styles.about_s1}>REFRESHINGLY DIFFERENT</h3>
+            <div className={styles.block1}>Bursting with West Country bittersweet cider apples, expertly crafted to our own special blend</div>
+          </div>
+
+      <div className={styles.bottom_p_1}>
+      <img src="\images\About Page Icon 2.png" className={styles.icon2} alt="pics from backend" />
+      <h3 className={styles.about_s2}>VEGAN FRIENDLY</h3>
+      <div className={styles.block2}>Suitable for vegans, vegetarians, fruitarians and even flexitarians...</div>
+      </div>
+
+            <div className={styles.bottom_p_2}>
+            <img src="\images\About Page Icon 3.png" className={styles.icon3} alt="pics from backend" />
+            <h3 className={styles.about_s3}>LESS BUBBLES, MORE APPLES</h3>
+            <div className={styles.block3}>Gently sparkling, perfect for pairing with food...</div>
         </div>
-      <div className={styles.bottom_p_3}></div>
-
-          <div className={styles.about_img}>
-            <img src="\images\Footer Image.jpg" className={styles.about1} alt="pics from backend" />
 
 
         </div>
+
+
       </section>
 
     </Fragment>
