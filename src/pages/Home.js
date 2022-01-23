a525b024b0ca33ea59d646ee81c857c031e1231e
Michael Hayes <mhayesstudy@gmail.com>
2022-01-23 16:11:24 +1100

    Combined Animation and Main branches

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,42 +4,22 @@ import { Link } from "react-router-dom";
 import CiderCarosel from ".././components/UI/CiderCarosel";
 import MerchCarosel from ".././components/UI/MerchCarosel";
 import SignUp from ".././components/UI/SignUp";
+import Button from ".././components/UI/Button";
 
-import styles from "./styles/static.module.css";
+import classes from "./styles/static.module.css";
+import styles from "./styles/Home.module.css";
 
 const Home = () => {
   return (
     <Fragment>
       <section className={(styles.cider_element, styles.darkblue)}>
-        <h2>Cider to snout about</h2>
-        <button>LEARN MORE</button>
+        <h2 className={styles.heading}>Cider to snout about</h2>
+        {/* <Button className={styled.btn1} type="submit">
+          LEARN MORE
+        </Button> */}
         <img
           src="./images/ciders/Homepage_Splash_Image_Cider.png"
           className={styles.bottle_size}
-          alt="pics from backend"
-        />
-      </section>
-
-      <section className={styles.cream}>
-        <h2>Rooted in Sommerset</h2>
-        <p>OUR CORE VALUES</p>
-        <div className={styles.dics}>
-          <div className={styles.disc}>1</div>
-          <div className={styles.disc}>2</div>
-          <div className={styles.disc}>3</div>
-        </div>
-        <button>ABOUT US</button>
-      </section>
-
-      <section className="farmRip">
-        <img
-          src="./images/backgrounds/Paper Rip Texture Downwards.png"
-          className={styles.tear_top}
-          alt="pics from backend"
-        />
-        <img
-          src="./images/backgrounds/Homepage Splash Image.jpg"
-          className={styles.torn_banner}
           alt="pics from backend"
         />
         <img
@@ -47,6 +27,54 @@ const Home = () => {
           className={styles.tear_bottom}
           alt="pics from backend"
         />
+      </section>
+
+      <section className={styles.cream}>
+        <h2 className={styles.heading2}>Rooted in Sommerset</h2>
+        <p className={styles.cream_p}>OUR CORE VALUES</p>
+        <div className={styles.dics}>
+          <img
+            src="./images/backgrounds/values.svg"
+            alt="Orchard Pig: BOLD,MISCHEVIOUS AND SIMPLE"
+          />
+          {/* <div className={styles.disc}>1</div>
+          <div className={styles.disc}>2</div>
+          <div className={styles.disc}>3</div>
+        </div>
+        <div className={styles.disc_title}>
+        <div className={styles.title}>BOLD</div>
+          <div className={styles.title}>MISCHEVIOUS</div>
+          <div className={styles.title}>SIMPLE</div> */}
+        </div>
+        {/* <Button className={styles.btn2} type="submit">
+          ABOUT US
+        </Button> */}
+      </section>
+
+      <section className="farmRip">
+        <slide className={styles.slide}>
+          <slide>
+            <img
+              src="./images/backgrounds/Paper Rip Texture Downwards.png"
+              className={styles.tear_top}
+              alt="pics from backend"
+            />
+          </slide>
+          <slide>
+            <img
+              src="./images/backgrounds/Homepage Splash Image.jpg"
+              className={styles.torn_banner}
+              alt="pics from backend"
+            />
+          </slide>
+          <slide>
+            <img
+              src="./images/backgrounds/Paper Rip Texture Upwards.png"
+              className={styles.tear_bottom}
+              alt="pics from backend"
+            />
+          </slide>
+        </slide>
       </section>
 
       <section className={styles.cream}>
@@ -61,9 +89,9 @@ const Home = () => {
         <MerchCarosel />
       </section>
 
-      <section>
+      {/* <section>
         <SignUp />
-      </section>
+      </section> */}
     </Fragment>
   );
 };
