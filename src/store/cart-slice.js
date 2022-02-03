f5e511deb9e1cdb89c8d21e1ab4dc2ac88de0731
Michael Hayes <mhayesstudy@gmail.com>
2022-02-03 13:23:45 +1100

    added reduce cart item and exported cart slice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import {createSlice} from '@reduxjs/toolkit';
+import { iteratorSymbol } from 'immer/dist/internal';
 
 createSlice({
     name: 'cart',
@@ -18,6 +19,21 @@ createSlice({
                 existingItem.quantity = existingItem.quantity + newItem.quantity;
                 existingItem.totalPrice = existingItem.totalPrice + newItem.totalPrice;
             }
+        },
+        removeItemFromCart(state, action){
+            const id = action.payload;
+            const existingItem = state.items.find(item=> item.id == id);
+            if(existingItem.quantity === 1){
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice =  existingItem.totalPrice - existingItem.price;
+            }
+
         }
     }
-})
+});
+
+export const cartActions = cartSlice.actions;
+
+export default createSlice;
--- src/store/storeIndex.js
+++ src/store/storeIndex.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import ageCheckSlice from "./age-check-slice";
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
-  reducer: { ageCheck: ageCheckSlice.reducer },
+  reducer: { ageCheck: ageCheckSlice.reducer, cart: cartSlice.reducer }
 });
 
 export default store;
