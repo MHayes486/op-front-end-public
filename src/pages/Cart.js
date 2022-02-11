2364d2d1542a5c18c72cd1013169fbc0f4235cb4
Michael Hayes <mhayesstudy@gmail.com>
2022-02-11 16:43:22 +1100

    Begin adding to backend

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,15 +1,28 @@
 import { useSelector } from "react-redux";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import CartItem from "../components/cart/CartItem";
+import { placeOrder } from "../lib/api";
+import useHttp from "../hooks/use-http";
 
 const Cart = () => {
+  const { sendRequest, status } = useHttp(placeOrder);
+
+  let nameValue = "";
+  let addressValue = "";
+  let emailAddress = "";
   const cartItems = useSelector((state) => state.cart.items);
   const totalCost = useSelector((state) => state.cart.totalFullPrice);
   const totalQuant = useSelector((state) => state.cart.totalQuantity);
-const avgCost = parseFloat(totalCost/totalQuant).toFixed(2);
-const shipping = 25.00;
-const totalCharge = parseFloat(totalCost) + shipping; 
+  const avgCost = parseFloat(totalCost / totalQuant).toFixed(2);
+  const shipping = 25.0;
+  const totalCharge = parseFloat(totalCost) + shipping;
 
+  const submitOrder = (e) => {
+    e.preventDefault();
+    const deliveryDetails = (nameValue, addressValue, emailAddress);
+    console.log(deliveryDetails);
+    console.log(cartItems);
+  };
 
   return (
     <Fragment>
@@ -34,10 +47,22 @@ const totalCharge = parseFloat(totalCost) + shipping;
           Total Cost: £{totalCost} ({totalQuant} items) || (£{avgCost} per
           item);
         </span>
-        <span>   ||  Flat shipping rate: £{shipping}, total cost =£{totalCharge}</span>
+        <span>
+          {" "}
+          || Flat shipping rate: £{shipping}, total cost =£{totalCharge}
+        </span>
       </div>
       <div></div>
       <div>Ship To:</div>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" value={nameValue}></input>
+        <label htmlFor="adress">Deliver Address:</label>
+        <input type="textarea" id="adress" value={addressValue}></input>
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="email" value={emailAddress}></input>
+        <button onClick={submitOrder}>Place order</button>
+      </form>
     </Fragment>
   );
 };
