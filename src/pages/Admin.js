78ce9400af65f4a6a7cef51b91e2556ac3ad910e
Adam Walker <18893036@student.curtin.edu.au>
2022-02-11 20:40:33 +1000

    Adding Admin

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,17 +1,46 @@
 import { Fragment, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+
+import useHttp from "../hooks/use-http";
+import { getAllMerch } from "../lib/api";
+
+import MerchList from "../components/admin/merch/MerchList";
+import Loading from "../components/Loading";
+
+import { SlSpinner } from "@shoelace-style/shoelace/dist/react";
 
 const Admin = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedMerch,
+    error,
+  } = useHttp(getAllMerch, true);
 
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
 
+  if (status === "pending") {
+    return <SlSpinner style={{ fontSize: "3rem" }} />;
+ 
+  }
 
-    return (
-        <Fragment>
-            <h1>Admin</h1>
+  if (error) {
+    return <p>{error}</p>;
+  }
 
-        </Fragment>
-    );
+  if (status === "completed" && (!loadedMerch || loadedMerch.length === 0)) {
+    return <p>No merch available?!?</p>;
+  }
+
+  return (
+    <Fragment>
+      <h1>Admin</h1>
+      <h4>Update Merchandise</h4>
+      <section>
+        <MerchList merch={loadedMerch} />
+      </section>
+    </Fragment>
+  );
 };
-
 export default Admin;
