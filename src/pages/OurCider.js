eb42766165fbbb0fddc2ebc4d28e4f094dbb713f
Adam Walker <18893036@student.curtin.edu.au>
2022-02-18 20:59:14 +1000

    Update

--- src/pages/OurCider.js
+++ src/pages/OurCider.js
@@ -4,7 +4,7 @@ import useHttp from "../hooks/use-http";
 import { getAllCiders } from "../lib/api";
 
 import CiderList from "../components/ciders/CiderList";
-
+import ciderStyles from "./styles/Cider.module.css";
 import { SlSpinner } from "@shoelace-style/shoelace/dist/react";
 
 const OurCider = () => {
@@ -36,10 +36,26 @@ const OurCider = () => {
 
   return (
     <Fragment>
-      <h2>Our Cider</h2>
-      <h3>This is what we are about</h3>
+
+      <section >
+        <img
+          className={ciderStyles.ciders_banner}
+          src="./images/backgrounds/Ciders Page Splash Image.jpg"
+          alt="pics from backend"
+        />
+      </section>
+
       <section>
+
+        <img
+          className={ciderStyles.ciders_tear_background}
+          src="\images\backgrounds\Home-Page-Rip-2.png"
+          alt="pics from backend"
+        />
+
+      <div >
         <CiderList cider={activeCiders} />
+        </div>
       </section>
     </Fragment>
   );
