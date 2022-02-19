c6ca8a14aca26535d2c8371ec394f192f2bacc79
Michael Hayes <mhayesstudy@gmail.com>
2022-02-19 20:51:10 +1100

    Bootstrap attempt for placing pairings in list group, NOT TESTED

--- src/components/pairings/PairingsList.js
+++ src/components/pairings/PairingsList.js
@@ -5,14 +5,15 @@ import classes from './PairingsList.module.css';
 
 const PairingsList = (props) => {
   return (
-    <ul className={classes['pairings-list']}>
+    // <ul className={classes['pairings-list']}>
+    <ul className="list-group list-group-horizontal-lg">
       {props.pairs.map((pairs) => (
         <Pairs
           key={pairs.id}
           pigProduct={pairs.pigProduct}
           dishName={pairs.dishName}
           ingredients={pairs.ingredients}
-          image={pairs.image}          
+          image={pairs.image}
         />
       ))}
     </ul>
--- src/components/pairings/Pairs.js
+++ src/components/pairings/Pairs.js
@@ -4,13 +4,23 @@ import classes from './Pairs.module.css';
 
 const Pairs = (props) => {
   return (
-    <li className={classes.pairs}>
-      <h2>{props.dishName}</h2>
-      <p><b>Pairs With:</b>
-        <h3>  {props.pigProduct}</h3></p>
-      <p><b>Ingredients:</b>  {props.ingredients}</p>
+    <li className="list-group-item">
+      <div className={classes.pairs}>
+        <h2>{props.dishName}</h2>
+        <p>
+          <b>Pairs With:</b>
+          <h3> {props.pigProduct}</h3>
+        </p>
+        <p>
+          <b>Ingredients:</b> {props.ingredients}
+        </p>
 
-      <img src={(`${props.image}`)} alt="pic from backend" className={classes.image} />
+        <img
+          src={`${props.image}`}
+          alt="pic from backend"
+          className={classes.image}
+        />
+      </div>
     </li>
   );
 };
