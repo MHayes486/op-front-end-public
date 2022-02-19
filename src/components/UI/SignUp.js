a7791f14ca2ab7878e4f55f90c976aa808d80268
Michael Hayes <mhayesstudy@gmail.com>
2022-02-19 20:35:12 +1100

    Changed back SignUp

--- src/components/UI/SignUp.js
+++ src/components/UI/SignUp.js
@@ -1,16 +1,41 @@
-import { Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import SignUpForm from "./SignUpForm";
 import styles from "./SignUp.module.css";
+import useHttp from "../../hooks/use-http";
+import { addMailListMember } from "../../lib/api";
+import { signUpCheckActions } from "../../store/signUp-check-slice";
+import { Fragment } from "react";
 
 const SignUp = () => {
+  const dispatch = useDispatch();
+  const signUpCheckStorage = localStorage.getItem("joined");
+  if (signUpCheckStorage === "1") {
+    dispatch(signUpCheckActions.validate());
+  }
+
+  const signUpValidated = useSelector((state) => state.signUpCheck.signedUp);
+  const { sendRequest } = useHttp(addMailListMember);
+
+  const signUpHandler = (signUpData) => {
+    sendRequest(signUpData);
+  };
+
   return (
-    <Fragment>
+    <div className={styles.wholeForm}>
       <h2>Connect with us</h2>
-      <p>
-        Please get in contact with any queries you have ana a member of our team
-        will get back to you soon.
-      </p>
-      <form className={styles.form}></form>
-    </Fragment>
+
+      {!signUpValidated && (
+        <Fragment>
+          {" "}
+          <p>
+            Please get in contact with any queries you have and a member of our
+            team will get back to you soon.
+          </p>
+          <SignUpForm onSignUp={signUpHandler} />
+        </Fragment>
+      )}
+      {signUpValidated && <h3>Thanks for joining the community!</h3>}
+    </div>
   );
 };
 
