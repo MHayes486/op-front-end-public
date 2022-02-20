1ab31949888b4b2558d7d17ee9d2f846dfd96af5
Michael Hayes <mhayesstudy@gmail.com>
2022-02-20 16:37:25 +1100

    Admin page updated

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -4,7 +4,8 @@ import AdminMerch from "../components/admin/AdminMerch";
 import AdminPairs from "../components/admin/AdminPairs";
 import AdminEvents from "../components/admin/AdminEvents";
 import AdminCiders from "../components/admin/AdminCiders";
-import AdminAdds from "../components/admin/AdminAdditions";
+import AdminOrders from "../components/admin/AdminOrders";
+import AdminContacts from "../components/admin/AdminContacts";
 
 import AddMerchs from "../components/merchandise/AddMerchs";
 import AddPairings from "../components/pairings/AddPairings";
@@ -29,23 +30,28 @@ const Admin = () => {
         <h4 className={styles.admin_h3}>Add a Pairing</h4>
         <AddPairings />
       </section>
-      <hr>
-      </hr>
-      {/* <section>
-        <h4>Update Events</h4>
+      <hr></hr>
+      <section>
+        {/* <h4 className={styles.admin_h3}>Update Events</h4>
         <AdminEvents />
-        <h4>Add an Event</h4>
-        <AddEvents />
+        <h4 className={styles.admin_h3}>Add an Event</h4>
+        <AddEvents /> */}
       </section>
       <hr></hr>
       <section>
-        <h4>Update Pairings</h4>
+        <h4 className={styles.admin_h3}>Update Ciders</h4>
         <AdminCiders />
-        <h4>Add a new Cider</h4>
+        <h4 className={styles.admin_h3}>Add a new Cider</h4>
         <AddCiders />
       </section>
-      <hr></hr> */}
-      <AdminAdds />
+      <hr></hr>
+      <section>
+        <h4 className={styles.admin_h3}>Orders List</h4>
+        <AdminOrders />
+        <hr></hr>
+        <h4 className={styles.admin_h3}>Contacts List</h4>
+        <AdminContacts />
+      </section>
     </Fragment>
   );
 };
