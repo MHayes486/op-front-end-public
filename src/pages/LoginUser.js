9f63e29c1d2dfa4a0fa550e16c34047b53e9262e
Michael Hayes <mhayesstudy@gmail.com>
2022-02-20 15:13:42 +1100

    Added login and Register user navigation buttons

--- src/pages/LoginUser.js
+++ src/pages/LoginUser.js
@@ -1,54 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { loginUser } from '../actions/userActions'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../actions/userActions";
 import styles from "./styles/Home.module.css";
 
 export default function LoginUser() {
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  // localStorage.setItem("merchUser", 1)
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const dispatch = useDispatch()
-    // localStorage.setItem("merchUser", 1)
-
-    useEffect(() => {
-        if (localStorage.getItem('currentUser')) {
-            window.location.href='/'
-        }
-    }, [])
-
-    function login() {
-        const user = {
-            email,
-            password
-        }
-        dispatch(loginUser(user))
+  useEffect(() => {
+    if (localStorage.getItem("currentUser")) {
+      window.location.href = "/";
     }
+  }, []);
 
-    return (
-        <div>
+  const onClickHandler = () => {
+    navigate("/register");
+  };
+
+  function login() {
+    const user = {
+      email,
+      password,
+    };
+    dispatch(loginUser(user));
+  }
+
+  return (
+    <div>
+      <div>
+        <div className="row justify-content-center mt-5">
+          <div className="col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
+            <h2 className={styles.home_merch_h2}>Login</h2>
             <div>
-                <div className="row justify-content-center mt-5">
-                    <div className="col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
-                        <h2 className={styles.home_merch_h2}>Login</h2>
-                        <div>
-                            <input required type="text" placeholder="email" className={styles.form_input} value={email} onChange={(e) => { setEmail(e.target.value) }} />
-                            <input
-                                required
-                                type="text"
-                                placeholder="password"
-                                className={styles.form_input}
-                                value={password}
-                                onChange={(e) => { setPassword(e.target.value) }} />
-                            <button className={styles.form_login_button} onClick={login}> LOGIN </button>
-                            <br/>
-                            <a style={{color: 'black'}} className={styles.form_register_button} href='/register'> Click here to register </a>
-                        </div>
-
-
-                    </div>
-                </div>
+              <input
+                required
+                type="text"
+                placeholder="email"
+                className={styles.form_input}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <input
+                required
+                type="text"
+                placeholder="password"
+                className={styles.form_input}
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <button className={styles.form_login_button} onClick={login}>
+                {" "}
+                LOGIN{" "}
+              </button>
+              <br />
+              <button onClick={onClickHandler}>Register a User</button>
+              {/* <a style={{color: 'black'}} className={styles.form_register_button} href='/register'> Click here to register </a> */}
             </div>
-
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
