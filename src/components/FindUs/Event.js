e504408941dc9aaab4c1b1702a55c13ce715f15d
Michael Hayes <mhayesstudy@gmail.com>
2022-02-20 20:14:51 +1100

    restyling

--- src/components/FindUs/Event.js
+++ src/components/FindUs/Event.js
@@ -4,7 +4,7 @@ import classes from './Event.module.css';
 
 const Event = (props) => {
   return (
-    <li className={classes.events}>
+    <li className={classes.event}>
       <h2>{props.eventName}</h2>
       <h3>{props.eventAddress}</h3>
       <img src={( `${props.image}` )} alt="pic from backend" className={classes.imageSize}/> 
