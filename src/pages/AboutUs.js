84c0d4643660e80aceca6732296c9a5342d80dce
kymlee-art <75294559+kymlee-art@users.norepy.github.com>
2022-02-21 22:13:06 +1100

    About page redid code to make it responsive added a grid also.

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -10,24 +10,6 @@ const AboutUs = () => {
         We like to poke fun at the world and ourselves...
       </h3>
 
-      <section >   {/* 
-        <div className={styles.cream}></div>
-        <h1 className={styles.about_us_h1}>Mischief in the Mainstream</h1>
-        <p className={styles.sub_heading}>
-          We like to poke fun at the world and ourselves...And Each Other{" "}
-        </p>*/}
-        <div className={styles.tear1}>
-          {/*         <img
-            className={(styles.tear_upwards, styles.tear_upward_img)}
-            src="\images\backgrounds\Paper Rip Texture Downwards.png"
-            alt="pics from backend"
-          />   */}
-          {/* <button className={styles.about_btn} onClick>
-            Learn More
-          </button> */}
-        </div>
-      </section>
-
       <section className={styles.About_us_2}>
         <div className={styles.about_img}>
           <img
@@ -38,65 +20,44 @@ const AboutUs = () => {
         </div>
       </section>
 
-      <section className={styles.About_us_3}>
-        <div className={styles.About2}>
-          <img
-            src="\images\backgrounds\Paper Rip Texture Upwards (Black).png"
-            className={styles.about_tear2}
-            alt="pics from backend"
-          />
+      <section className={styles.about_section}>
+        <div className={styles.about3_inner}>
+          <figure className={styles.about_img3}>
+            <img
+              src="\images\backgrounds\About Page Image 1.jpg"
+              alt="Glass of beer"
+            />
+            <img
+              src="\images\backgrounds\About Page Image 2.jpg"
+              alt="Bottle of Hog Father"
+            />
+          </figure>
+
+          <article className={styles.about_pigstail_text}>
+            <h2 className={styles.about_heading2}>A PIG'S TAIL</h2>
+            <p className={styles.about_pg1}>
+              It all started in the naughties, when Andrew and Neil were enjoying
+              their home-made cider and hog's roast with friends...
+            </p>
+            <p className={styles.about_pg2}>
+              When our founder started dabbing with cider making in his garden
+              shed, he found that West Country Apples make great tasting cider.
+              Following this happy discovery, Orchard Pig arrived out of a shared
+              thrist for great tasting cider, great food and old spots, The
+              Original Orchard Pigs.
+            </p>
+          </article>
         </div>
       </section>
 
-      <section className={styles.About_us_4}>
-        <div className={styles.about_img3}>
-          <img
-            src="\images\backgrounds\About Page Image 1.jpg"
-            className={styles.about1}
-            alt="pics from backend"
-          />
-          <img
-            src="\images\backgrounds\About Page Image 2.jpg"
-            className={styles.about2}
-            alt="pics from backend"
-          />
-        </div>
-        <h2 className={styles.about_heading2}>A PIG'S TAIL</h2>
-        <div className={styles.about_pigstail_text}>
-          <p className={styles.about_pg1}>
-            It all started in the naughties, when Andrew and Neil were enjoying
-            their home-made cider and hog's roast with friends...
-          </p>
-          <p className={styles.about_pg2}>
-            When our founder started dabbing with cider making in his garden
-            shed, he found that West Country Apples make great tasting cider.
-            Following this happy discovery, Orchard Pig arrived out of a shared
-            thrist for great tasting cider, great food and old spots, The
-            Original Orchard Pigs.
-          </p>
-        </div>
-      </section>
-
-      <section className={styles.About_us_5}>
-        <div className={styles.tear4}>
-          <img /*
-            src="\images\backgrounds\Paper Rip Texture Downwards (Black).png"
-            className={styles.about_tear3}
-            alt="pics from backend"
-          *//>
-          <div className={styles.about_img5}>
+      <section className={styles.about_tear_image}>
+          <figure className={styles.about_img5}>
             <img
               src="\images\backgrounds\About Page Splash Image 2 Torn.jpg"
               className={styles.about5}
               alt="pics from backend"
             />
-          </div>
-          <img
-            src="\images\backgrounds\Paper Rip Texture Upwards.png"
-            className={styles.about_tear5}
-            alt="pics from backend"
-          />
-        </div>
+          </figure>
       </section>
 
       <section className={styles.About_us_6}>
@@ -155,3 +116,4 @@ const AboutUs = () => {
 };
 
 export default AboutUs;
+
